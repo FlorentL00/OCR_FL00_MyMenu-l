a9cc9e1b3b0a22786ba02730243c3286d7372ed1
FlorentL00 <florent0710@gmail.com>
2018-12-24 16:57:15 +0100

    Ajout option dans le menu - method displayAvailableMenu - method displaySelectedMenu

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,17 +1,23 @@
 package com.ocr.anthony;
 
+import java.sql.SQLOutput;
+
 public class Order {
     /**
      * Display all available menus in the restaurant.
      */
     public void displayAvailableMenu() {
-
+        System.out.println("Choix du menu");
+        System.out.println("1 - poulet");
+        System.out.println("1 - boeuf");
+        System.out.println("3 - végétarien");
+        System.out.println("Que souhaitez-vous comme menu?");
     }
     /**
      * Display a selected menu.
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-
+        System.out.println("Vous avez choisi le menu" + nbMenu);
     }
 }
